24cde88bb0746a40a2834e610481b1ebfc19c563
Серьоженко Дмитро <13789697@gsuite.duit.edu.ua>
2025-11-20 16:10:22 +0200

    Add files via upload

---
+++ abstract-factory.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+using namespace std;
+
+// aбстрактний продукт //
+class IMessage {
+public:
+    virtual void show() const = 0;
+    virtual ~IMessage() = default;
+};
+
+// конкретні продукти //
+class HelloMessage : public IMessage {
+public:
+    void show() const override {
+        cout << "hello" << endl;
+    }
+};
+
+class ByeMessage : public IMessage {
+public:
+    void show() const override {
+        cout << "goodbye" << endl;
+    }
+};
+class IMessageFactory {
+public:
+    virtual IMessage* createMessage() const = 0;
+    virtual ~IMessageFactory() = default;
+};
+
+class HelloFactory : public IMessageFactory {
+public:
+    IMessage* createMessage() const override {
+        return new HelloMessage();
+    }
+};
+
+class ByeFactory : public IMessageFactory {
+public:
+    IMessage* createMessage() const override {
+        return new ByeMessage();
+    }
+};
+
+int main() {
+    HelloFactory hello;
+    ByeFactory bye;
+
+    IMessage* msg1 = hello.createMessage();
+    IMessage* msg2 = bye.createMessage();
+
+    msg1->show();
+    msg2->show();
+
+    return 0;
+}
